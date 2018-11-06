f4f90be3c3aab2f3bdcfe7eb0794aaa0c3834754
Mariane <mahrinoa@gmail.com>
2018-11-06 22:52:14 +0100

    finish backend jwt and authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,7 +1,21 @@
 const User = require('../models/user');
+const jWt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+	const timestamp = new Date().getTime();
+	return jWt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+	res.send({ token: tokenForUser(req.user) });
+};
+
 exports.signup = function(req, res, next) {
 	const email = req.body.email;
 	const password = req.body.password;
+
+	if (!email || !password) return res.status(422).send({ message: 'You must provide email and password' });
 	//check if user exists
 	User.findOne({ email: email }, function(err, existingUser) {
 		console.log(existingUser);
@@ -23,6 +37,6 @@ exports.signup = function(req, res, next) {
 			}
 		});
 		//return message
-		res.json({ success: true });
+		res.json({ success: true, token: tokenForUser(user) });
 	});
 };
--- router.js
+++ router.js
@@ -1,8 +1,13 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignIn = passport.authenticate('local', { session: false });
 module.exports = function(app) {
+	app.get('/', requireAuth, function(req, res) {
+		res.send({ hi: 'there' });
+	});
+	app.post('/signin', requireSignIn, Authentication.signin);
 	app.post('/signup', Authentication.signup);
-	// app.get('/test1', function(req, res, next) {
-	// 	console.log('boo');
-	// 	res.send([ 'bla', 'cactus', 'tv' ]);
-	// });
 };
